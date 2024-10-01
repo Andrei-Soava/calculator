25ef89c24ea8a04cc171c43852530ea002b42011
LocatelliFabio <61050236+LocatelliFabio@users.noreply.github.com>
2024-10-01 16:17:00 +0200

    Miglioramento

--- skibidi/src/main/java/calculator/skibidi/App.java
+++ skibidi/src/main/java/calculator/skibidi/App.java
@@ -9,6 +9,7 @@ public class App
 	static int F =10;
     public static void main( String[] args )
     {
+	throw new Exception();
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
