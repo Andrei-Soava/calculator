8bc5206137ff76d27cfde6972163dd90c662be7f
Andrei-Soava <andreisoava02@gmail.com>
2024-10-01 16:09:05 +0200

    Update App.java

--- skibidi/src/main/java/calculator/skibidi/App.java
+++ skibidi/src/main/java/calculator/skibidi/App.java
@@ -6,8 +6,10 @@ package calculator.skibidi;
  */
 public class App 
 {
+	static int F =10;
     public static void main( String[] args )
     {
+        System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
 }
